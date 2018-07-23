6558a909156fe665e6af3b1aebfae3c58e3198a9
Kleber Anderson <kleber7777@gmail.com>
2018-07-23 11:56:52 -0300

    teste github pages

---
+++ docs/precache-manifest.2453e11204d4107d5a4063fce45ecb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "019d306dbbaf0ac04c49",
+    "url": "/js/chunk-vendors.019d306d.js"
+  },
+  {
+    "revision": "dd072db648d68303c7f4",
+    "url": "/js/app.dd072db6.js"
+  },
+  {
+    "revision": "597e791b9af5b0999bcc17a82edbf876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dd072db648d68303c7f4",
+    "url": "/css/app.bc780402.css"
+  }
+];
