e0ab15e40a30d45941f94684727da8025d6238c3
Kleber Anderson <kleber7777@gmail.com>
2018-07-23 23:32:42 -0300

    Adição de Pug e Sass

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Teste from '@/components/Teste'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/teste',
+      name: 'Teste',
+      component: Teste
     }
   ]
 })
